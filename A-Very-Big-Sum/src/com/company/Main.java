1ec15ee69e6ed892b2cc29ca7d325a8d7765f86f
garu12 <garu12@hotmail.com>
2021-02-08 23:31:24 -0500

    Ciclo for each suma de numeros Long muy grandes

---
+++ A-Very-Big-Sum/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    // Complete the compareTriplets function below.
+    static long aVeryBigSum(long[] ar) {
+        long sum = 0L;
+        for (long l : ar) {
+            sum += l;
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
