0c8f70d8acb01905e51bfb7530836e411740fee8
garu12 <garu12@hotmail.com>
2021-03-21 20:53:52 -0500

    Problema

---
+++ Number-line-jumps/src/com/company/Solution.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2){
+
+        if (v1 != v2){
+            String result = (v1 > v2 && x1 < x2 ) ?
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
